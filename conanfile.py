a0e8169de762a561cb8d16c6eb7d78485d2455e2
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-07-23 17:10:13 +0200

    Add conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -5,59 +5,53 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    # Indicates License type of the packaged library
-    license = "MIT"
+class libsocket(ConanFile):
+    name = "libsocket"
+    version = "2.4.1"
+    description = "Conan.io Package for libsocket Libary."
+    url = "https://github.com/l0nax/conan-libsocket"
+    homepage = "https://github.com/dermesser/libsocket"
+    author = "Emanuel Bennici <benniciemanuel78@gmail.com>"
+    license = "https://github.com/dermesser/libsocket/blob/master/LICENSE"
 
-    # Packages the license for the conanfile.py
-    exports = ["LICENSE.md"]
+    exports = ["LICENSE"]
 
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
 
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    options = dict({
+        "shared":       [True, False],
+        "only_cpp":     [True, False],
+        "only_c":       [True, False],
+        "fPIC":         [True, False],
+    })
 
-    # Custom attributes for Bincrafters recipe conventions
+    default_options = "shared=False", "static=False", "fPIC=True"
+    build_policy = "missing"
+
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
-
     def config_options(self):
         if self.settings.os == 'Windows':
-            del self.options.fPIC
+            raise Exception("This Libary does not support Windows!")
+        if self.settings.os == 'MacOS':
+            raise Exception("This Libary does not support Mac OS!")
+
+        if self.settings.only_cpp && self.settings.only_c:
+            raise Exception("Please choose if you would this Library with C++- or C-only Support.")
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/dermesser/libsocket"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
     def configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        cmake.definitions["BUILD_STATIC_LIB"] = (True if self.settings.static else False)
         cmake.configure(build_folder=self.build_subfolder)
         return cmake
 
@@ -69,15 +63,6 @@ class LibnameConan(ConanFile):
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
         cmake = self.configure_cmake()
         cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
