3f9a656cbb09bded7cc0c69407fdaeb827ded2d4
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-07-23 17:29:37 +0200

    Use conan build Tools in 'build.py'

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder()
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
     builder.run()
