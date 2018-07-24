902f58a877fc78149d6154a3b8f806739b1be2dc
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-07-24 11:52:59 +0200

    ** UNDOCUMENTED **

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(reference="libsocket/2.4.1@l0nax/stable")
     builder.run()
