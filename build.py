3f750d7753bf320881d648b1a64dcca209f2a39b
SSE4 <tomskside@gmail.com>
2018-10-09 23:35:36 +0700

    - update travis
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from cpt.packager import ConanMultiPackager
+
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(reference="libsocket/2.4.1@l0nax/stable")
+
+    builder = build_template_default.get_builder()
+
     builder.run()
