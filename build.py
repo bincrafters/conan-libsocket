8399dfa5aaf698ef6490a3155130928097c4f229
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-10-11 18:19:25 +0200

    Try to Fix python Error

--- build.py
+++ build.py
@@ -4,7 +4,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-	builder = ConanMultiPackager(username='');
+	builder = ConanMultiPackager('');
 	
 	builder.add_common_builds()
 	builder.run()
