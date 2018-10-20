908626c56c513442f6b5cb50a12a214fc8272689
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-10-20 15:16:39 +0200

    Fix Platform Incompatibility

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,7 +3,7 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 from conans import ConanFile, CMake
-import os
+import os, platform
 
 
 class TestPackageConan(ConanFile):
@@ -16,6 +16,17 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        # check if we haved Cross-Compiled
-        if not os.path.isfile('./CROSS_COMPILING'):
-            self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+        ## check if Binary can be executed
+        p = subprocess.Popen(["file", os.path.join("bin", "test_package")], stdout=subprocess.PIPE, shell=True)
+        output = p.communicate()[0]
+        
+        arch = platform.architecture()
+        
+        ## only Execute if it's possible
+        ## it's not releay a nice way..
+        if arch == "64bit":
+            if "64-bit" in output and "x86-64" in output: 
+                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
+         elif arch == "32bit":
+            if "32-bit" in output and "x86" in output:
+                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
