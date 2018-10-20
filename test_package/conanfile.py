22a5c0f984593aa424a276f53f09881821ecaa8f
Emanuel Bennici <benniciemanuel78@gmail.com>
2018-10-20 19:04:59 +0200

    Fix Python Code...

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
 from conans import ConanFile, CMake
 import os, platform
-
+import subprocess
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -27,6 +26,6 @@ class TestPackageConan(ConanFile):
         if arch == "64bit":
             if "64-bit" in output and "x86-64" in output: 
                 self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
-         elif arch == "32bit":
+        elif arch == "32bit":
             if "32-bit" in output and "x86" in output:
                 self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","test_package")))
