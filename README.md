5063adeab9d5b11ea272c7fdc62f371ae8f60d6f
bincrafters-user <bincrafters@gmail.com>
2019-09-24 01:59:14 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.11.2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 | Bintray   | Linux Build |
 | :--------:|:-----------------:|
-| [ ![Download](https://api.bintray.com/packages/bincrafters/public-conan/libsocket%3Abincrafters/images/download.svg) ](https://bintray.com/bincrafters/public-conan/libsocket%3Abincrafters/_latestVersion) | [![Build Status](https://travis-ci.org/bincrafters/conan-libsocket.svg?branch=testing%2F2.4.1)](https://travis-ci.org/bincrafters/conan-libsocket) |
+| [ ![Download](https://api.bintray.com/packages/bincrafters/public-conan/libsocket%3Abincrafters/images/download.svg) ](https://bintray.com/bincrafters/public-conan/libsocket%3Abincrafters/_latestVersion) | [![Build Status](https://travis-ci.com/bincrafters/conan-libsocket.svg?branch=testing%2F2.4.1)](https://travis-ci.com/bincrafters/conan-libsocket) |
 
 ## Conan.io Information
 
--- conanfile.py
+++ conanfile.py
@@ -26,48 +26,48 @@ class libsocket(ConanFile):
         "out_":         ['all', 'cpp', 'c']
     })
 
-    default_options = "shared=False", "fPIC=True", "out_=all"
+    default_options = {'shared': False, 'fPIC': True, 'out_': 'all'}
     build_policy = "missing"
 
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
 
     def source(self):
-        self.run("git clone https://github.com/dermesser/libsocket --depth 1 %s" % self.source_subfolder)
+        self.run("git clone https://github.com/dermesser/libsocket --depth 1 %s" % self._source_subfolder)
 
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
 
         return cmake
 
 
     def build(self):
         if not self.options.shared:
-            tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+            tools.replace_in_file(os.path.join(self._source_subfolder, 'CMakeLists.txt'),
                                   'ADD_DEPENDENCIES(socket++ socket)',
                                   'ADD_DEPENDENCIES(socket++_int socket)')
-            tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+            tools.replace_in_file(os.path.join(self._source_subfolder, 'CMakeLists.txt'),
                                   'export(TARGETS socket++ socket_int',
                                   'export(TARGETS socket++_int socket_int')
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'C++', 'CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'C++', 'CMakeLists.txt'),
                               'SET(CMAKE_CXX_FLAGS "-std=c++11") # -DVERBOSE")',
                               'SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11") # -DVERBOSE")')
 
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.install()
 
         # copy LICENSE File
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
 
 
     def package_info(self):
